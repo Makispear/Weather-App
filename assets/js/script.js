c2345ad80ee17edd77a4a75ee06ddae2b6b54766
Maki Aboabida <maki-miko@hotmail.com>
2021-08-26 14:55:48 -0400

    HELP REQUEST(IGNORE COMMIT): bug - can't console.log() fetched Data

---
+++ assets/js/script.js
@@ -0,0 +1,36 @@
+let SearchInput = document.getElementById('cityNameInput')
+let searchBtn = document.getElementById('searchBtn');
+let searchFormEl = document.getElementById('searchFormEl');
+let apiKey = 'a162d79bf40c41aa937d1346397ac5c6'
+
+let getWeatherApi = (SearchInput) => {
+    let apiUrl = `api.openweathermap.org/data/2.5/weather?q=${SearchInput}&appid=${apiKey}`
+    fetch(apiUrl)
+    .then((response) => {
+        if (response.ok) {
+            response.json()
+            .then((data) => {
+                console.log(data)
+            })
+        } else {
+            alert(`Error: Information Not Found!`)
+        }
+    })
+    .catch((error) => {
+        alert(`Sorry couldn't get information. Please Try agin or Contact us.`);
+      });
+    
+}
+
+let formSubmitHandler = (e) => {
+    e.preventDefault()
+    let cityName = SearchInput.value.trim()
+    if (cityName) {
+        console.log(cityName)
+        getWeatherApi(cityName)
+    } else {
+        alert("Please Enter a Valid City Name!")
+    }
+}
+
+searchFormEl.addEventListener('submit', formSubmitHandler)
